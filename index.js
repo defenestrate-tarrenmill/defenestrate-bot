990bab7a2a1f9a26e0c929f8277ed0cf1373ed38
defenestrate-tarrenmill <cf6383@my.open.ac.uk>
2018-06-18 19:51:29 +0100

    Added health check API for openshift

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const discord = require('discord.js');
 const client = new discord.Client();
 const ChatService = require('./services/ChatService');
 const chatService = new ChatService(client);
+const express = require('express');
+const healthCheck = express();
 
-
+healthCheck.get('/health',(req, res)=>{
+    res.send({'status':'ok'});
+});
+healthCheck.listen(3000,()=>{
+    console.log('health check online');
+});
 client.on('ready',()=>{
     console.log('defenestrate bot js is online');
 });
